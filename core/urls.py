55d779a6816af661434db9fbdf50386d37c8e89a
madalbekovich <talasbekalgaziev@gmail.com>
2024-11-05 16:43:07 +0600

    test continios

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', admin.site.urls),
+    path('dashboard2/', admin.site.urls),
     path('v1.0/house/', include('apps.house.urls')),
     path('v1.0/main/', include('apps.main.urls')),
     path('v2/auth/', include('apps.accounts.urls')),
